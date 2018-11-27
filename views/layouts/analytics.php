96763eab272fa265bfac92ec7870f69f0dc268cd
Dmitro Ovsiichuk <d.ovsiichuk@digitalscreens.com.ua>
2018-11-27 10:59:00 +0200

    added sitemap

--- views/layouts/analytics.php
+++ views/layouts/analytics.php
@@ -1,9 +1,11 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-89816712-3"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+<?php if (config('app')['env'] == 'production'):?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-89816712-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-    gtag('config', 'UA-89816712-3');
-</script>
+        gtag('config', 'UA-89816712-3');
+    </script>
+<?php endif; ?>
